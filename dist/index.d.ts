152f27817cfb4a0cfd15bac947af55c09bbbf4df
Terry Fei <i@terryfei.com>
2022-07-07 18:30:31 +0800

    rm dist

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="hyper-function-component" />
-declare const _default: (HFC: typeof HyperFunctionComponent) => import("vue").DefineComponent<{}, () => any[], {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, import("vue").EmitsOptions, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<{} & {} & {}>, {}>;
-export default _default;
--- dist/to-vue.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="hyper-function-component" />
-import type IVue from "vue";
-export default function (Vue: typeof IVue): (HFC: typeof HyperFunctionComponent) => IVue.DefineComponent<{}, () => any[], {}, {}, {}, IVue.ComponentOptionsMixin, IVue.ComponentOptionsMixin, IVue.EmitsOptions, string, IVue.VNodeProps & IVue.AllowedComponentProps & IVue.ComponentCustomProps, Readonly<{} & {} & {}>, {}>;
